c088611c4438f7c6beb851e1ee13e36b7c04d309
kahnu044 <kahnucharan044@gmail.com>
2024-08-14 16:23:47 +0530

    Google oauth2 setup

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const { google } = require('googleapis');
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 8000;
-
 const app = express();
+
+// Define the scope of access for the Google Calendar API.
+const scopes = ['https://www.googleapis.com/auth/calendar'];
+
+// OAuth 2 configuration
+const oauth2Client = new google.auth.OAuth2
+    (
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URL
+    );
+
 
 // Routes
 app.get("/", (req, res) => {
@@ -11,6 +23,29 @@ app.get("/", (req, res) => {
         status: true,
         message: "Welcome to our backend Server"
     });
+});
+
+app.get('/auth', (req, res) => {
+    const url = oauth2Client.generateAuthUrl
+        ({
+            access_type: 'offline',
+            scope: scopes
+        });
+    res.redirect(url);
+});
+
+app.get("/auth/redirect", async (req, res) => {
+
+    const { tokens } = await oauth2Client.getToken(req.query.code);
+    // {
+    //     access_token: 'ya29.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX_8rMsiYDSYpbY-cQPL_bj1o1SSjeLSYiVjCQr86BqHpMXXXXXXXXXcHVreGSkL1RQIEq3TA6bMswjhQuaiRDuqUeOJoh5DsGnSgkIks15jYEGh5rSNRhKdfuS0oj_tVrhgvJx9obu8oZaCgYKAa4SARESFQHGX2MicrNJHscz9fs0oZyPrkj6qQ0171',
+    //     refresh_token: '1//0g8PJ36zZ-NxMXXXXXXXXXXXXAAGBASNwF-L9IrlCzT_rlZ9SXXXXXXXXXXXXXXXR_LeNFntd4PKsiFnEE3DqYkXXXXXXXXXXLjG9vU',
+    //     scope: 'https://www.googleapis.com/auth/calendar',
+    //     token_type: 'Bearer',
+    //     expiry_date: 1723636310687
+    // }
+    oauth2Client.setCredentials(tokens);
+    res.send('Authentication successful! Please return to the console.');
 });
 
 app.listen(PORT, () => {
